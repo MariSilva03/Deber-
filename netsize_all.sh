11b7fe3c2e6a19358a9bff7b992861363a824515
MariSilva03 <94406745+MariSilva03@users.noreply.github.com>
2021-12-10 17:58:42 -0500

    modifiqué algunos puntos

--- netsize_all.sh
+++ netsize_all.sh
@@ -1,3 +1,6 @@
 #Deber_2
 
 for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
+En nano realice un conteo general de los archivos 
+El uso del for permite repetir una tarea con diferentes  variaciones 
+El codigo desarrollado se corre en el git bash
