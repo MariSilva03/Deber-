d458295a43a88e9303fda9531b95f64cf4e8020e
MariSilva03 <94406745+MariSilva03@users.noreply.github.com>
2021-12-10 17:54:01 -0500

    Update netsize.sh
    Le modifiqué algunos pasos

--- netsize.sh
+++ netsize.sh
@@ -5,5 +5,4 @@ cat n10.txt
 cat ../Saavedra2013/n10.txt | wc -l
  
 head -n1 ../Saavedra2013/n10.txt | tr -d " " | tr -d "\ n" | wc -c
-
-for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
+En el readme le coloqué los códigos 
