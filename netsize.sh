35345cdb8c4a5e27c7e5fcec56266c03c75c8e26
MariSilva03 <94406745+MariSilva03@users.noreply.github.com>
2021-12-10 00:15:56 -0500

    Update netsize.sh
    Agregue los códigos programados en gitbash y separe las filas

--- netsize.sh
+++ netsize.sh
@@ -1,5 +1,9 @@
 Deber N° 1  de  Bioinformática  "
+
 cat n10.txt
+ 
 cat ../Saavedra2013/n10.txt | wc -l
+ 
 head -n1 ../Saavedra2013/n10.txt | tr -d " " | tr -d "\ n" | wc -c
+
 for file in $(ls *.txt); do wc -l $file;  head -n1 $file | grep -o " " | wc -l; done
